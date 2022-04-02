c5f9fc6b8d2c2595caee468d0f585a61a8246474
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-04-02 22:32:14 +0300

    Job done in TodoList and main method

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,30 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
+        String deals;
+        String nameOfDeals;
+        String regex = "ADD";
+        String regex2 ="LIST";
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        deals = scanner.nextLine();
+        if(deals.equals(regex)){
+            nameOfDeals = scanner.nextLine();
+            todoList.add(nameOfDeals);
+            System.out.println("Добавлено дело "+ todoList.getTodos());
+        }
+        else if (deals.equals(regex2)){
+            System.out.println(todoList.getTodos());
+
+        }
+
         // TODO: написать консольное приложение для работы со списком дел todoList
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -3,29 +3,42 @@ package practice;
 import java.util.ArrayList;
 
 public class TodoList {
+    public ArrayList<String> todolist = new ArrayList<>();
 
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        todolist.add(todo);
+        // TODO:добавьте переданное дело в конец списка
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
+        if (index < todolist.size() && index >= 0) {
+            todolist.add(index, todo);
+        } else {
+            todolist.add(todo);
+        }
+
+        // TODO: добавьте дело на указанный индекс,
         //  проверьте возможность добавления
     }
 
     public void edit(int index, String todo) {
+        if (index < todolist.size() && index > 0) {
+            todolist.set(index, todo);
+        }
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
     }
 
     public void delete(int index) {
+        if (index < todolist.size() && index > 0) {
+            todolist.remove(index);
+        }
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+        return todolist;
     }
 
 }
